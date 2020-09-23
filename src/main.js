e8fab08407db9096d5e4bdb6a820aaa931cc9b5c
John Lyu <paleneutron@outlook.com>
2020-09-23 10:43:12 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -14,6 +14,6 @@ new Vue({
   render: h => h(App),
   created() {
     // Prevent blank screen in Electron builds
-    this.$router.push('/')
+    this.$router.push('/').catch(()=>{});
   }
 }).$mount('#app')
